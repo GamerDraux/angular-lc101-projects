a6c9a398ca7d5c9318994adfc40856adac591ffd
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-27 10:33:51 -0500

    after playing with movies ngFor command

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8', 'Cloverfield', 'District 9'];
+   movies = ['Frozen', 'The Avengers', 'Congo(not really)', 'The Manchurian Candidate', 'Oceans 8', 'Cloverfield', 'District 9'];
 
    constructor() { }
 
