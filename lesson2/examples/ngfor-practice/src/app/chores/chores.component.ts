667a40236a97a3971804bdf7cc1ce98eb703885b
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-27 11:06:38 -0500

    added some if/else statements to blocks

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   chores = [
+      {title: 'Yesterday\'s Chores', tasks: ['Pick up dog poop', 'Complete LaunchCode prep work']},
+      // tslint:disable-next-line: max-line-length
+      {title: 'Today\'s Chores', tasks: ['Pick up dog poop', 'Clean bathroom', 'Get groceries you forgot', 'Complete LaunchCode prep work']},
+      {title: 'Tomorrow\'s Chores', tasks: ['Pick up dog poop', 'Clean bathroom', 'Complete LaunchCode prep work']},
+      {title: 'Chores for next week', tasks: ['Pick up dog poop', 'Clean bathroom', 'Complete LaunchCode prep work']}
+   ];
 
    constructor() { }
 
